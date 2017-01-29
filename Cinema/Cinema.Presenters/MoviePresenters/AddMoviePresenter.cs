70c7a22420c72dc85c8f69fa498c4813874c73fe
iliangogov <iliangogov@gmail.com>
2017-01-29 12:28:44 +0200

    Presenters project added

---
+++ Cinema/Cinema.Presenters/MoviePresenters/AddMoviePresenter.cs
@@ -0,0 +1,13 @@
+﻿using Cinema.Presenters.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Presenters.MoviePresenters
+{
+    public class AddMoviePresenter:IAddMoviePresenter
+    {
+    }
+}
