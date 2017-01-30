0cbceb615ede4eefab3241e5846bfebb6d9ac006
iliangogov <iliangogov@gmail.com>
2017-01-30 21:41:41 +0200

    Sample Cinema Hall schema added

--- Cinema/Cinema.Web/MovieDetailsView.aspx.cs
+++ Cinema/Cinema.Web/MovieDetailsView.aspx.cs
@@ -19,6 +19,8 @@ namespace Cinema.Web
         [Inject]
         public IMovie Movie { get; set; }
 
+        private IList<ImageButton> buttons = new List<ImageButton>();
+
         private string queryId;
 
         protected void Page_PreLoad(object sender, EventArgs e)
@@ -31,6 +33,33 @@ namespace Cinema.Web
         {
             this.MovieImg.ImageUrl = this.Movie.ImageUrl;
             this.MovieImg.Width = 300;
+            this.buttons.Add(ImageButton0);
+            this.buttons.Add(ImageButton1);
+            this.buttons.Add(ImageButton2);
+            this.buttons.Add(ImageButton3);
+            this.buttons.Add(ImageButton4);
+            this.buttons.Add(ImageButton5);
+            this.buttons.Add(ImageButton6);
+            this.buttons.Add(ImageButton7);
+            this.buttons.Add(ImageButton8);
+        }
+
+        protected void Button0_Click(object sender, EventArgs e)
+        {
+            var button = sender as ImageButton;
+
+            var senderIndex = int.Parse(button.ID.Substring(button.ID.Length - 1));
+
+            var clickedbutton = this.buttons[senderIndex];
+
+            if (clickedbutton.BackColor == System.Drawing.Color.LemonChiffon)
+            {
+                clickedbutton.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                clickedbutton.BackColor = System.Drawing.Color.LemonChiffon;
+            }
         }
     }
 }
--- Cinema/Cinema.Web/MovieDetailsView.aspx.designer.cs
+++ Cinema/Cinema.Web/MovieDetailsView.aspx.designer.cs
@@ -22,12 +22,93 @@ namespace Cinema.Web {
         protected global::System.Web.UI.WebControls.Image MovieImg;
         
         /// <summary>
-        /// Calendar1 control.
+        /// Field control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+        protected global::System.Web.UI.WebControls.Table Field;
+        
+        /// <summary>
+        /// ImageButton0 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton0;
+        
+        /// <summary>
+        /// ImageButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+        
+        /// <summary>
+        /// ImageButton2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton2;
+        
+        /// <summary>
+        /// ImageButton3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton3;
+        
+        /// <summary>
+        /// ImageButton4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton4;
+        
+        /// <summary>
+        /// ImageButton5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton5;
+        
+        /// <summary>
+        /// ImageButton6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton6;
+        
+        /// <summary>
+        /// ImageButton7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton7;
+        
+        /// <summary>
+        /// ImageButton8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton8;
     }
 }
