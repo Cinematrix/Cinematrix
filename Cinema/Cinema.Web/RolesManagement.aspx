db39166ea230513890840729f8df10734e75b92d
iliangogov <iliangogov@gmail.com>
2017-02-14 09:52:40 +0200

    Roles management view implemented

---
+++ Cinema/Cinema.Web/RolesManagement.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="RolesManagement.aspx.cs" Inherits="Cinema.Web.RolesManagement" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron" style="background-color: rgba(255, 255, 255, 0.8)">
+        <asp:DropDownList ID="UsersDropdown" runat="server"></asp:DropDownList>
+        <p>
+            <asp:Button ID="submitButton" runat="server" Text="Make admin" CssClass="btn btn-success" />
+        </p>
+    </div>
+</asp:Content>
--- Cinema/Cinema.Web/Site.Master.designer.cs
+++ Cinema/Cinema.Web/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace Cinema.Web {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl PaymentLink;
         
         /// <summary>
+        /// RolesLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl RolesLink;
+        
+        /// <summary>
         /// ProfilePicture control.
         /// </summary>
         /// <remarks>
