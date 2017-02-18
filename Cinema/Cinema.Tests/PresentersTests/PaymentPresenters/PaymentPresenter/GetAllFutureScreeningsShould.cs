95120e7d72ffb565a41d1930b87fb03d3be87ed6
iliangogov <iliangogov@gmail.com>
2017-02-18 12:10:55 +0200

    PaymentPresenter/GetAllFutureScreenings method tested

---
+++ Cinema/Cinema.Tests/PresentersTests/PaymentPresenters/PaymentPresenter/GetAllFutureScreeningsShould.cs
@@ -0,0 +1,40 @@
+﻿using Cinema.Data.Models;
+using Cinema.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Cinema.Tests.PresentersTests.PaymentPresenters.PaymentPresenter
+{
+    [TestFixture]
+    public class GetAllFutureScreeningsShould
+    {
+        [Test]
+        public void CallScreeningServiceGetAllFutureScreeningsMethod()
+        {
+            var mockedScreeningService = new Mock<IFilmScreeningService>();
+            var mockedSeatService = new Mock<ISeatService>();
+
+            var actualPaymentPresenter =
+                new Presenters.PaymentPresenters.PaymentPresenter(mockedScreeningService.Object, mockedSeatService.Object);
+
+            actualPaymentPresenter.GetAllFutureScreenings();
+
+            mockedScreeningService.Verify(service => service.GetAllFutureScreenings(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnIQueryableFilmScreeningCollection()
+        {
+            var mockedScreeningService = new Mock<IFilmScreeningService>();
+            var mockedSeatService = new Mock<ISeatService>();
+
+            var actualPaymentPresenter =
+                new Presenters.PaymentPresenters.PaymentPresenter(mockedScreeningService.Object, mockedSeatService.Object);
+
+            var result = actualPaymentPresenter.GetAllFutureScreenings();
+
+            Assert.That(result, Is.Not.Null.And.InstanceOf<IQueryable<FilmScreening>>());
+        }
+    }
+}
