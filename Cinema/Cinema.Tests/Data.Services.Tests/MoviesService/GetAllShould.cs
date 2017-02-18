b834392b04ded8227d47f3d120831e6c8ccb2936
iliangogov <iliangogov@gmail.com>
2017-02-18 17:29:10 +0200

    MoviesService/GetAll method tested

---
+++ Cinema/Cinema.Tests/Data.Services.Tests/MoviesService/GetAllShould.cs
@@ -0,0 +1,42 @@
+﻿using Cinema.Data.Models;
+using Cinema.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Tests.Data.Services.Tests.MoviesService
+{
+    [TestFixture]
+    public class GetAllShould
+    {
+        [Test]
+        public void CallMovieRepositoryAllMethod()
+        {
+            var mockedMovieRepo = new Mock<IRepository<Movie>>();
+
+            var actualMoviesService =
+                new Cinema.Data.Services.MoviesService(mockedMovieRepo.Object);
+
+            actualMoviesService.GetAll();
+
+            mockedMovieRepo.Verify(service => service.All(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnIQueryableMoviesCollectionProvidedFromMovieRepositoryAllMethod()
+        {
+            var mockedMovieRepo = new Mock<IRepository<Movie>>();
+
+            var actualMoviesService =
+                new Cinema.Data.Services.MoviesService(mockedMovieRepo.Object);
+
+            var result = actualMoviesService.GetAll();
+
+            Assert.That(result, Is.Not.Null.And.InstanceOf<IQueryable<Movie>>());
+        }
+    }
+}
