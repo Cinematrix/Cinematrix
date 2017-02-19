e0d75c7439916ff99b2abfc168dc733d08680cac
iliangogov <iliangogov@gmail.com>
2017-02-19 12:23:48 +0200

    FilmScreeningService/GetAllScreenings method tested

---
+++ Cinema/Cinema.Tests/Data.Services.Tests/FilmScreeningService/GetAllScreeningsShould.cs
@@ -0,0 +1,40 @@
+﻿using Cinema.Data.Models;
+using Cinema.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Cinema.Tests.Data.Services.Tests.FilmScreeningService
+{
+    [TestFixture]
+    public class GetAllScreeningsShould
+    {
+        [Test]
+        public void CallFilmScreeningRepositoryAllMethod()
+        {
+            var mockedScreeningRepo = new Mock<IRepository<FilmScreening>>();
+            var mockedFilmSCreening = new Mock<FilmScreening>();
+
+            var actualFilmSCreeningService =
+                new Cinema.Data.Services.FilmScreeningService(mockedScreeningRepo.Object, mockedFilmSCreening.Object);
+
+            actualFilmSCreeningService.GetAllScreenings();
+
+            mockedScreeningRepo.Verify(service => service.All(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnIQueryableFilmScreeningsCollectionFromRepositoryAllMethod()
+        {
+            var mockedScreeningRepo = new Mock<IRepository<FilmScreening>>();
+            var mockedFilmSCreening = new Mock<FilmScreening>();
+
+            var actualFilmSCreeningService =
+                new Cinema.Data.Services.FilmScreeningService(mockedScreeningRepo.Object, mockedFilmSCreening.Object);
+
+            var result = actualFilmSCreeningService.GetAllScreenings();
+
+            Assert.That(result, Is.Not.Null.And.InstanceOf<IQueryable<FilmScreening>>());
+        }
+    }
+}
